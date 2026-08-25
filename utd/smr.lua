07791a9bc6105aa35a4f6778871367e319d542de
lghft <121789924+lghft@users.noreply.github.com>
2026-08-25 13:23:47 -0700

    Update smr.lua

--- utd/smr.lua
+++ utd/smr.lua
@@ -1343,9 +1343,9 @@ function replayMatch()
         getgenv().Ability = true
         autoTowerAbility(3, 1) 
         task.wait()
+        placeUnit(5, Vector3.new(-1510.7905273438, 143.55992126465, -1384.5667724609), 1)--4
         autoTowerAbility(2, 1)
         task.wait()
-        placeUnit(5, Vector3.new(-1510.7905273438, 143.55992126465, -1384.5667724609), 1)--4
         placeUnit(3, Vector3.new(-1166.2114257812, 135.05136108398, -1308.1571044922), 1)--5  -1166.2114257812, 135.05136108398, -1308.1571044922
         print("should've ulq")
         --Notify("Print", "[Wave Action] >>> WAVE 1 COMPLETED <<<")
@@ -1503,12 +1503,12 @@ function summerMatch()
         --Notify("Print", "[Wave Action] >>> EXECUTING WAVE 1 FUNCTION <<<")
         task.wait(1)
         placeUnit(1, Vector3.new(-1177.3988037109, 136.49728393555, -1315.2202148438), 0)--3 -1177.3988037109, 136.49728393555, -1315.2202148438
+        placeUnit(5, Vector3.new(-1510.7905273438, 143.55992126465, -1384.5667724609), 1)--4
         getgenv().Ability = true
         autoTowerAbility(3, 1) 
         task.wait()
         autoTowerAbility(2, 1)
         task.wait()
-        placeUnit(5, Vector3.new(-1510.7905273438, 143.55992126465, -1384.5667724609), 1)--4
         placeUnit(3, Vector3.new(-1166.2114257812, 135.05136108398, -1308.1571044922), 1)--5  -1166.2114257812, 135.05136108398, -1308.1571044922
         print("should've ulq")
         --Notify("Print", "[Wave Action] >>> WAVE 1 COMPLETED <<<")
